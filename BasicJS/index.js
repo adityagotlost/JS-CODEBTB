bc0238526e9caa2d48d201bc223ee32bb18f32d8
adityagotlost <adityagotlost@users.noreply.github.com>
2026-06-06 13:09:21 +0530

    Initial commit

---
+++ BasicJS/index.js
@@ -0,0 +1,47 @@
+//console.log(`Hello`);
+//console.log(`I like Anime`);
+//window.alert(`this is an alert`);
+//window.alert(`i hate no one`);
+//document.getElementById("myh1").textContent = `Hello`;
+//document.getElementById("myp").textContent = `I like Anime`;
+
+// 1. declaration let x;
+// 2. assignment x = 100;
+
+/*
+let x;
+x = 100;
+console.log(x);
+*/
+
+/*
+let age = 19;
+let price = 10.99;
+
+
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+*/
+
+/*
+let firstName = "Adi";
+let favfood = "Pizza";
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`You like ${favfood}`);
+*/
+
+/*
+let online = false;
+let forSale = true;
+console.log(`Bro is online: ${online}`);
+console.log(`Is This Car For Sale: ${forSale}`);
+*/
+
+let fullName = "Aditya";
+let age = 25;
+let student = true;
+
+document.getElementById("p1").textContent = `your name is ${fullName}`;
+document.getElementById("p2").textContent = `you are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
