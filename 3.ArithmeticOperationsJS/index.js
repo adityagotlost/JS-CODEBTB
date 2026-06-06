a65dda64afb88676855317ad58825e6e680f9cbc
adityagotlost <adityagotlost@users.noreply.github.com>
2026-06-07 03:39:13 +0530

    added AO and AUI

---
+++ 3.ArithmeticOperationsJS/index.js
@@ -0,0 +1,30 @@
+// arithmetic operators = operands (value, variables, etc.)
+//                        operators (+ - * /)
+//                        ex. 11 = x + 5;
+
+//let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 1;
+//students = students / 1;
+//students = students ** 2;
+
+//let extraStudents = students % 4;
+
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//students **=2;
+//students %= 2;
+
+
+//students++;
+//students--;
+
+//console.log(students);
+
+let result = 1 + 2 * 3 + 4 ** 2;
+console.log(result);
