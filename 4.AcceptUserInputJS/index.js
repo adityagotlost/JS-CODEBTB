0f1777ebc4b249e55b0d6f5f726d2450b99f96ba
adityagotlost <adityagotlost@users.noreply.github.com>
2026-06-07 03:56:30 +0530

    updated AUI

--- 4.AcceptUserInputJS/index.js
+++ 4.AcceptUserInputJS/index.js
@@ -10,3 +10,10 @@ username = window.prompt("what's your username?");
 console.log(username);
 */
 
+
+let username;
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = `hello ${username}`;
+}
