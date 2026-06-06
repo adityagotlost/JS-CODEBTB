3339297dd581caded8102585f7347fafe4fa8818
adityagotlost <adityagotlost@users.noreply.github.com>
2026-06-06 13:11:45 +0530

    update folder name

---
+++ 1.BasicJS/index.js
@@ -0,0 +1,7 @@
+//console.log(`Hello`);
+//console.log(`I like Anime`);
+//window.alert(`this is an alert`);
+//window.alert(`i hate no one`);
+//document.getElementById("myh1").textContent = `Hello`;
+//document.getElementById("myp").textContent = `I like Anime`;
+
