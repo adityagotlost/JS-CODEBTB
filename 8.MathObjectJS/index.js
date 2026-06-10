821d16ec9fc639b8503c396f6696507c7e5fe847
adityagotlost <adityagotlost@users.noreply.github.com>
2026-06-10 22:29:04 +0530

    feat: add counter program with HTML, CSS, and JavaScript implementation

---
+++ 8.MathObjectJS/index.js
@@ -0,0 +1,62 @@
+//MATH = built-in object that provides a 
+//       collection of properties and methods
+
+
+//console.log(Math.PI);
+//console.log(Math.E);
+
+
+/*
+let x = 3.21;
+let y = 2;
+let z;
+
+
+//z = Math.round(x);
+// round off
+
+//z = Math.floor(x);
+//floor always round down
+
+//z = Math.ceil(x);
+//floor always round up
+
+//z = Math.trunc(x);
+//eleminate decimal protion
+
+//z = Math.pow(x,y);
+//x power y
+
+//z = Math.sqrt(x);
+//square of a no.
+
+
+//z = Math.log(x);
+//log of a no.
+
+//z = Math.sin(x);
+//sin x
+
+//z = Math.cos(x);
+//cos x
+
+//z = Math.tan(x);
+//tan x
+
+//z = Math.abs(x);
+//used to eleminate negative sign
+
+//z = Math.sign(x);
+//tell us about the sign -ve +ve (-1,0,1)
+*/
+
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+let max = Math.max(x,y,z);
+let min = Math.min(x,y,z);
+
+console.log(min);
+console.log(max);
