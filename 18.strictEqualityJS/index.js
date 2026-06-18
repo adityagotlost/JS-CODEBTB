b98500ba9eb9aeeb5fc17d5d439fc80e8e48c7ca
adityagotlost <adityagotlost@users.noreply.github.com>
2026-06-18 14:02:47 +0530

    fix: improve formatting and clarity of strict equality explanation

--- 18.strictEqualityJS/index.js
+++ 18.strictEqualityJS/index.js
@@ -8,11 +8,13 @@
 // != (Loose)	Yes	            No	                Yes
 // !== (Strict)	Yes	            Yes	                No
 
+
+// you should almost always use !== (and its partner ===).Because != tries to guess and convert types behind the scenes, it can lead to really weird, hard - to - find bugs in your code.Using !== forces you to be explicit and keeps your code predictable!
 const PI = 3.14;
 
-if(PI === "3.14"){
+if (PI === "3.14") {
     console.log("That is NOT Pi");
 }
-else{
+else {
     console.log("That is Pi");
 }
